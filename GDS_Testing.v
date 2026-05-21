b2c9f8da9eba22c918e3512ead4257b83dab0389
LozanoCyrielle <cyrielle.lozano@epfl.ch>
2026-05-21 16:36:05 +0200

    reproved simple runStmt

--- GDS_Testing.v
+++ GDS_Testing.v
@@ -40,147 +40,70 @@ Theorem runVar :
     runStmtP 10 $0 $0 
     ((var "a" := Const 2) ;; (var "b" := Const 3) ;; 
     (var "ret" := ((Var "a") + (Var "b"))) ;; skip) = Some (vg2, vl2)
-    -> (vg2 $? "ret" = Some (varAss 5)).
+    -> (vl2 $? "ret" = Some (varAss 5)).
 Proof.
     intros.
     inversion H.
-    repeat special_match.
-    inversion H1; repeat special_match.
-    clear H1; clear H; inversion H0; repeat special_match.
-    clear H0; inversion H; repeat special_match.
-    clear H; inversion H0.
-    destruct H as [? [? ?]]; simpl in H; subst.
-    clear H0.
-    inversion H2.
-    destruct H as [? [? ?]]; simpl in H; subst. clear H2.
-    inversion H1.
-    destruct H as [? [? ?]]; simpl in H.
-    assert ("a" <> "b") by congruence.
-    epose proof (lookup_add_ne _ _ H3) as Hlook.
-    
-    rewrite Hlook in H.
-    assert ("a" = "a") by reflexivity.
-    epose proof (lookup_add_eq _ _ H4) as Hlookne.
-    rewrite Hlookne in H.
-    assert ("b" = "b") by reflexivity.
-    epose proof (lookup_add_eq _ _ H5) as Hlookeq.
-    rewrite Hlookeq in H.
-    simpl in H; subst.
-    assert ("ret" = "ret") by reflexivity.
-    epose proof (lookup_add_eq _ _ H).
-    apply H0.
+    repeat program_match.
+    rewrite lookup_add_ne in H1 by discriminate.
+    rewrite lookup_add_eq in H1 by reflexivity.
+    rewrite lookup_add_eq in H1 by reflexivity.
+    injection H1 as Hvg2 Hvl2.
+    subst vl2.
+    rewrite lookup_add_eq by reflexivity; reflexivity.
 Qed.
 
 (*Pas encore redéfini, si preuve de RunVar réussie, redéfinir de la même manière*)
 Theorem runIf :
-    forall v2, 
-    (runStmt 10 $0 $0 
+    forall vg2 vl2, 
+    (runStmtP 10 $0 $0 
     ((( (var "a" := Const 2 ;; var "b" := Const 3) ;; 
     when ((Var "b") - (Var "a")) 
     then (var "ret" := Const 10) 
     else (var "ret" := Const 5) done) ) ;; 
-    skip)
-    $0 v2 )
-    -> v2 $? "ret" = Some (varAss 10).
+    skip)) = Some (vg2, vl2)
+    -> (vl2 $? "ret" = Some (varAss 10)).
 Proof.
     intros.
-    inversion H; repeat special_match.
-    inversion H0; repeat special_match.
-    clear H0; inversion H2; repeat special_match.
-    clear H2; inversion H3.
-    -repeat special_match.
-    clear H3; inversion H0; inversion H4.
-    clear H0 H4.
-    destruct H2 as [? [? ?]], H3 as [? [? ?]]; simpl in H2, H3; subst.
-    inversion H1; subst.
-    inversion H6; destruct H0 as [? [? ?]]; simpl in H0; subst.
-    assert ("ret" = "ret") by reflexivity.
-    epose proof (lookup_add_eq _ _ H0).
-    apply H2.
-    
-    - clear H3; inversion H0; inversion H4.
-    clear H0 H4.
-    destruct H3 as [? [? ?]], H5 as [? [? ?]]; simpl in H3, H5; subst.
-    destruct H2; simpl in H2.
-    simpl in H0.
-    assert ("a" <> "b") by congruence.
-    epose proof (lookup_add_ne _ _ H3) as Hlookne.
-    rewrite Hlookne in H0.
-    assert("a" = "a") by reflexivity.
-    epose proof (lookup_add_eq _ _ H4) as Hlooka.
-    rewrite Hlooka in H0.
-    rewrite lookup_add_eq in H0; simpl in H0.
-        + discriminate H0.
-        + reflexivity.
+    inversion H.
+    rewrite lookup_add_eq in H1 by reflexivity.
+    rewrite lookup_add_ne in H1 by discriminate.
+    rewrite lookup_add_eq in H1 by reflexivity.
+    simpl in H1.
+    injection H1 as Hvg2 Hvl2.
+    subst vl2.
+    rewrite lookup_add_eq by reflexivity; reflexivity.
 Qed.
 
 Theorem runWhile :
-    forall v2, 
-    (runStmt 10 $0 $0 
+    forall vg2 vl2, 
+    (runStmtP 10 $0 $0 
     ((((var "a" := Const 2 ;; while (Var "a") loop ("a" <- Var "a" - Const 1) done) ;; 
-    var "ret" := Var "a" + Const 5) ;; skip) )
-    $0 v2) 
-    -> (v2 $? "ret" = Some (varAss 5)).
+    var "ret" := Var "a" + Const 5) ;; skip))) = Some (vg2, vl2)
+    -> (vl2 $? "ret" = Some (varAss 5)).
 Proof.
-    intros v_res H.
-    inversion_clear H; repeat special_match.
-    inversion_clear H; repeat special_match.
-    inversion_clear H; repeat special_match.
-    inversion_clear H. destruct H3 as [? [? ?]]; simpl in H; subst.
-    assert (ha: "a" = "a") by reflexivity; 
-    epose proof (lookup_add_eq _ _ ha) as Hlooka.
-    inversion_clear H2; [repeat special_match | do 2 special_match].
-    
-    - (* h3 = assign -> (*Check local val, if exists, reassign, else check global, if exists reassign, else crash (prop is false)*) *)
-    inversion H3. 
-        + (* Check local val, if exists, reassign, *)
-        destruct H as [h1 [n [h3 [h4 h5]]]].
-        simpl in h3.
-        rewrite Hlooka in h3.
-        subst.
-        clear H3 H2 x3 h1.
-        inversion H4. 
-        -- destruct H as [inta [vgmid [vlmid [h1 [h2 [h3 h4]]]]]].
-        inversion h3. 
-        ++ 
-            destruct H as [h1' [n [h3' [h4' h5']]]].
-            simpl in h3'; rewrite (lookup_add_eq _ _ ha) in h3'; subst.
-            clear h3 h1' H4 h2.
-            inversion_clear h4.
-            * destruct H as [inta [vgmid' [vlmid [h1 [h2 [h3 h4]]]]]].
-            simpl in h1.
-            rewrite (lookup_add_eq _ _ ha) in h1.
-            congruence.
-            * destruct H as [h [h1 h2]].
-            subst.
-            inversion H1. 
-            destruct H as [h1 [h2 h3]].
-            inversion H0.
-            subst. simpl.
-            rewrite (lookup_add_eq _ _ ha).
-            assert (hret : "ret" = "ret") by reflexivity.
-            rewrite (lookup_add_eq _ _ hret).
-            reflexivity.
-        ++
-            destruct H as [h1' [h2' h3']].
-            rewrite (lookup_add_eq _ _ ha) in h2'; congruence.
-        -- simpl in H. epose proof (lookup_add_eq _ _ ha) as Hlookaa; rewrite Hlookaa in H; destruct H; discriminate.
-        + (* check global, knowing doesnt exist locally, if global exists reassign, *)
-        destruct H as [h1 [h2 h3]].
-        rewrite Hlooka in h2; congruence.
-    - simpl in H.
-    rewrite Hlooka in H; discriminate. 
+    intros.
+    inversion H.
+    rewrite lookup_add_eq in H1 by reflexivity.
+    rewrite lookup_add_eq in H1 by reflexivity.
+    simpl in H1.
+    rewrite lookup_add_eq in H1 by reflexivity.
+    rewrite lookup_add_eq in H1 by reflexivity.
+    injection H1 as Hvg2 Hvl2.
+    subst vl2.
+    rewrite lookup_add_eq by reflexivity; reflexivity.
 Qed.
 
 Theorem globalExe1 :
-    forall v2, (run 10 $0 
+    forall vg2, (runP 10 $0 
     ((((topVar "a" := Const 9 ;;; topVar "ret" := Const 0 ) ;;;
     methodDecl "plusOne" nil (Some "dump")  (var "dump" := (Var "a" + Const 1))) ;;;
-    readyDecl (assignCallMethodStmt (Some "ret") "plusOne" nil)) ;;; EndDecl)
-    v2) 
-    ->(v2 $? "ret" = Some (varAss 10)).
+    readyDecl (assignCallMethodStmt (Some "ret") "plusOne" nil)) ;;; EndDecl)) = Some vg2 
+    ->(vg2 $? "ret" = Some (varAss 10)).
 Proof.
-    intros.
+    (* intros.
+    simpl in H.
+    inversion H.
     repeat program_match.
     destruct H6.
     do 6 program_match.
@@ -211,7 +134,7 @@ Proof.
     rewrite lookup_add_eq in H2 by reflexivity.
     discriminate. 
 
-    (* Show Proof. *)
+    Show Proof. *)
 Qed.
 
 Theorem globalExe2 :
